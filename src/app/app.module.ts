309cb23d5bbee440f8ec0d202ed7fd6efc448426
DolphiNexus <jairo73456@gmail.com>
2019-02-14 09:01:24 -0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -8,18 +10,53 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { WeatherService } from './weather.service';
+
+import {
+  MatInputModule,
+  MatPaginatorModule,
+  MatProgressSpinnerModule,
+  MatSortModule,
+  MatTableModule,
+  MatIconModule,
+  MatButtonModule,
+  MatCardModule,
+  MatFormFieldModule } from "@angular/material";
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { LocalForecastComponent } from './local-forecast/local-forecast.component';
+import { FahrenheitPipe } from './fahrenheit.pipe';
+import { MomentPipe } from './moment.pipe';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, LocalForecastComponent, LocalForecastComponent, FahrenheitPipe, MomentPipe],
   entryComponents: [],
   imports: [
     BrowserModule,
     IonicModule.forRoot(),
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    DragDropModule,
+    ScrollingModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule,
+    MatSortModule,
+    MatTableModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    HttpClientJsonpModule,
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    WeatherService,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+
+  private appId: string;
+  private appCode: string;
+
+  public weather: any;
+
+  public constructor(private http: HttpClient) {
+    this.appId = "8jmIcMRZ5dDIuZqi7xDy";
+    this.appCode = "GsY8bsdL-liCTU0KexRlnQ";
+    this.weather = [];
+}
+
+  public ngOnInit() { 
+    if(navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+          this.getWeather(position.coords);
+      });
+  } else {
+      console.error("The device does not support geolocation...");
+  }
+  }
+
+  public getWeather(coordinates: any) {
+    this.http.jsonp("https://weather.cit.api.here.com/weather/1.0/report.json?product=forecast_7days_simple&latitude=" + coordinates.latitude + "&longitude=" + coordinates.longitude + "&app_id=" + this.appId + "&app_code=" + this.appCode, "jsonpCallback")
+        .pipe(map(result => (<any>result).dailyForecasts.forecastLocation))
+        .subscribe(result => {
+            this.weather = result.forecast;
+        }, error => {
+            console.error(error);
+        });
+  }
 
 }
+
+
